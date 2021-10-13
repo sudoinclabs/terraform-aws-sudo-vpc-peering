e5d317be4fbb78c9871b69d9d25b9bcee0a4abc3
Hameedullah Khan <hameed@sudo.inc>
2021-10-14 01:45:11 +0300

    Remove route53 submodule

--- modules/route53/README.md
+++
@@ -1,2 +0,0 @@
-# sudo-terraform-module-template
-Template repository for Terraform modules
--- modules/route53/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "domain_identity_arn" {
-  value = var.domain_identity != "" ? module.ses.domain_identity_arn : ""
-}
-output "dkim_tokens" {
-  value = module.ses.dkim_tokens
-}
